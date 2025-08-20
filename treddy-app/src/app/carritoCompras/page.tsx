13d3cdc5cbb72346026d897655694fd7f191ed8e
samuelchalarca.m <samuelchalarca.m@gmail.com>
2025-08-20 11:11:26 -0500

    Nav y footer por llamado

---
+++ treddy-app/src/app/carritoCompras/page.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react';
+
+export default function HomePage() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+
+
+return (
+    <main></main>
+)}
---
+++ treddy-app/src/pages/nav.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Nav() {
+    return(
+        <header className="flex justify-between items-center px-8 py-6 bg-[#0A0F2C] border-b border-[#1a1f40]">
+        <h1 className="text-2xl font-bold text-[#00E6F6]">TREDDY</h1>
+        <nav className="space-x-6">
+          <Link href="#" className="hover:text-[#00E6F6]">Inicio</Link>
+          <Link href="#" className="hover:text-[#00E6F6]">Productos</Link>
+          <Link href="#" className="hover:text-[#00E6F6]">Personalizar</Link>
+          <Link href="#" className="hover:text-[#00E6F6]">Contacto</Link>
+        </nav>
+        <div className="space-x-4">
+          <button>🔍</button>
+          <button>👤</button>
+          <button>🛒</button>
+        </div>
+      </header>
+    )
+}
