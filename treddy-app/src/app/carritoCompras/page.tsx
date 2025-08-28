11fd37ca94cc9df749d0a590e76b25b013945ad6
samuelchalarca.m <samuelchalarca.m@gmail.com>
2025-08-28 11:23:56 -0500

    Carrito compras organizado y vinculado a base de datos

--- treddy-app/src/app/carritoCompras/page.tsx
+++ treddy-app/src/app/carritoCompras/page.tsx
@@ -1,13 +1,20 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getFiguras } from '../../lib/api';
+import Image from "next/image";
 import Nav from "../../pages/nav";
 import Footer from "../../pages/footer";
 
 export default function Carrito() {
-  const [figuras, setFiguras] = useState<any[]>([
-  ]);
+  const [figuras, setFiguras] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const total = figuras.reduce(
+    (suma:number,figura: any) => suma + Number(figura.precio_base),
+    0
+  )
+  useEffect(() => {
+    getFiguras().then(setFiguras).catch(console.error);
+  }, []);
 
   return (
     <main className="min-h-screen bg-[#0A0F2C] text-white flex flex-col">
@@ -15,10 +22,10 @@ export default function Carrito() {
       <Nav />
 
       
-      <div className="flex-grow flex flex-col items-center justify-center px-4">
+      <div className="flex-grow flex flex-col items-center justify-center px-4 mt-10">
         <h2 className="text-2xl font-bold mb-6 text-center">Tu carrito</h2>
 
-        <div className="bg-[#0F173A] w-full max-w-md rounded-2xl shadow-lg p-8">
+        <div className="bg-[#0F173A] w-full max-w-md rounded-2xl shadow-lg p-8 py-10 mb-10">
           {figuras.length === 0 ? (
             
             <p className="text-center text-[#B5B8C5]">
@@ -28,13 +35,15 @@ export default function Carrito() {
             <>
               
               <div className="space-y-4">
-                {figuras.map((p, i) => (
+                {figuras.map((figura: any) => (
                   <div
-                    key={i}
+                    key={figura.id}
                     className="flex justify-between items-center bg-[#1a214f] p-3 rounded-lg"
                   >
-                    <p>{p.nombre}</p>
-                    <span>${p.precio}</span>
+                    <Image src={figura.imagenUrl} alt={figura.nombre}  width={70}
+                        height={70}></Image>
+                    <p>{figura.nombre}</p>
+                    <span>${figura.precio_base}</span>
                   </div>
                 ))}
               </div>
@@ -43,7 +52,7 @@ export default function Carrito() {
               <div className="mt-6 flex justify-between text-lg font-semibold">
                 <span>Total</span>
                 <span>
-                  ${figuras .reduce((acc, p) => acc + p.precio, 0)}
+                  ${total}
                 </span>
               </div>
 
