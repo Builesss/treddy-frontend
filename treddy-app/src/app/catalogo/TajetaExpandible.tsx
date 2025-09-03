e09510d997b5f6ef819f5d49229c3ae495e0d6b8
Cristopher Castaño} <criscastangar@gmail.com>
2025-09-03 14:57:06 -0500

    Agregado de boton stock y desplazamiento para camara

--- treddy-app/src/app/catalogo/TajetaExpandible.tsx
+++ treddy-app/src/app/catalogo/TajetaExpandible.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Image from 'next/image';
+import { useState, useRef, useEffect } from 'react'
 
 type Figura = {
   id: number
@@ -11,20 +12,41 @@ type Figura = {
 }
 
 export default function TarjetaExpandible({ figura, onClose }: { figura: Figura, onClose: () => void }) {
+  const [mostrarAR, setmostrarAR] = useState(false)
+  const videoRef = useRef<HTMLVideoElement | null>(null)
+
+    useEffect(() => {
+    if (mostrarAR && videoRef.current) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream
+          }
+        })
+        .catch((err) => console.error("Error accediendo a la cámara:", err))
+    }
+  }, [mostrarAR])
+
   if (!figura) return null
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
       <div className="bg-[#0F173A] p-6 rounded-xl max-w-md w-full text-center relative text-white">
         <button
+          onClick={() => setmostrarAR(!mostrarAR)}
+          className="absolute top-3 left-3 bg-gray-600 text-white font-semibold py-2 rounded-lg hover:bg-[#00E6F6] hover:text-black transition"
+        >
+          AR
+        </button>
+        <button
           onClick={onClose}
-          className="absolute top-3 right-3 text-white text-lg hover:text-[#00E6F6]"
+          className="absolute top-3 right-3 text-white py-2 rounded-lg  hover:text-[#00E6F6]"
         >
           X
         </button>
-         <p className="text-[#00E6F6] font-bold mt-1">Disponible: {figura.stock}</p>
+        <p className="text-[#00E6F6] font-bold mt-1">Disponible: {figura.stock}</p>
         <Image
-          src={figura.imagenUrl || "/placeholder.png"} 
+          src={figura.imagenUrl || "/placeholder.png"}
           alt={figura.nombre}
           width={350}
           height={400}
@@ -45,6 +67,12 @@ export default function TarjetaExpandible({ figura, onClose }: { figura: Figura,
           </button>
         </div>
       </div>
+
+      {mostrarAR && (
+          <div className="w-1/2 bg-black rounded-xl flex items-center justify-center">
+            <video ref={videoRef} autoPlay playsInline className="w-full h-full object-cover rounded-xl" />
+          </div>
+        )}
     </div>
   )
 }
