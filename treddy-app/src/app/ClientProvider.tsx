8e848d0bac5a4771f6402e6cafc71f142a89e70c
Sebastian Builes <sebasbuiles12@hotmail.com>
2025-10-23 11:19:34 -0500

    Se ajusta la gestion de productos con el Crud integrado

--- treddy-app/src/app/ClientProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { ReactNode } from "react";
-import { ProductosProvider } from "@/context/ProductosContext";
-
-export default function ClientProvider({ children }: { children: ReactNode }) {
-  return <ProductosProvider>{children}</ProductosProvider>;
-}
